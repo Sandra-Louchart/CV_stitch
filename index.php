211d0218e52589a51c23693a006029dac666d8fb
AliNedjar <alinedjarali45@outlook.com>
2020-09-30 10:27:06 +0200

    Travail sur la Homepage

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cv de Stitch</title>
+</head>
+<body>
+<img src="" alt="PhotoDeProfil">
+<h1>Expérience-628 "Stitch"</h1>
+<p>titre du poste: Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Assumenda atque beatae blanditiis corporis dolor dolorum
+    fuga itaque, neque nobis odio officiis quam quis quisquam sequi soluta temporibus vel velit voluptate.</p>
+
+<a>Expérience</a>
+<a>Compétence</a>
+<a>formation</a>
+<a>recomandation</a>
+<a>Contact</a>
+
+</body>
+</html>
