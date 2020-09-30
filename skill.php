95cdc7eef0983e787325a78dccddbda4e15ff0df
calouvts <pascalevetois@gmail.com>
2020-09-30 14:04:53 +0200

    createskill

---
+++ skill.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mes études et compétences</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+
+<body>
+
+
+<h1>Mes Compétences</h1>
+
+
+<section class="compétences">
+    <h2> Mes compétences professionnelles </h2>
+    <div class="professionalskill">
+        <ul>
+            <li>originaire de tantalogue, maitrise de 20 langue différententes, je suis donc particulièrement qualifié pour enseigner les langues étrangères</li>
+            <li>intelligence cybernétique, toujours un coup d'avance sur les élèves/li>
+            <li></li>
+            <li></li>
+
+        </ul>
+    </div>
+
+
+    <h2>Mes compétences personnelles</h2>
+
+    <div class=" personnalskill">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+        </ul>
+
+    </div>
+    <h2>xxx</h2>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</section>
+/*Le prototype 626, plus connu sous le nom de Stitch, est issu d’une expérience génétique illégale
+conduite par Jumba Jookiba et les protagonistes du film d’animation Disney Lilo & Stitch. Il est conçu
+pour être malicieux, et sa fonction première était de détruire tout ce qu’il touchait. Ses pouvoirs incluent
+une immense intelligence et une force phénoménale. Le personnage est ignifuge et à l’épreuve des balles.
+Il est doté de la vision nocturne, la vision par rayon X, et la vision infrarouge.
+Cet inséparable compagnon de Lilo est très apprécié des amoureux de la série.
+Malgré sa force surhumaine et son haut niveau d’intelligence cybernétique, Stitch est émotionnellement fragile
+et une personnalité enfantine se cache sous son extérieur monstrueux. Lilo croit que c’est l’une des raisons pour
+lesquelles,
+il a d’abord poursuivi son œuvre de destruction. Il a été conçu comme un monstre, mais a reçu peu ou pas de
+compréhension et de sympathie
+de la part des autres.
+
+Le personnage s’assagit après son amélioration, mais est bien souvent incapable de contrôler toutes ses pulsions
+et ses actions antisociales. Heureusement pour lui, Lilo (et plus tard Reuben et Angel) est toujours là pour le
+soutenir.
+Les téléspectateurs croient parfois que Lilo est dépendante de Stitch pour sa protection. Au contraire, c’est ce dernier
+qui est dépendant de Lilo.
+Malgré ses difficultés, Lilo fait tout son possible pour aider Stitch. Et à mesure que l’histoire progresse, celui-ci
+devient plus conscient et est
+capable de prendre les bonnes décisions. À la fin de la série, Stitch a des amis, de la famille, des êtres chers et un
+but dans lequel croire*/
+
+
+Tout en expliquant sa création au Conseil Galactique au début de Lilo & Stitch, le Dr Jumba donne le résumé suivant des
+pouvoirs de Stitch: « Il est à l’épreuve des balles, ignifuge, antichoc, et peut penser plus vite qu’un super
+ordinateur. Il voit dans l’obscurité et peut déplacer des objets faisant 3000 fois sa taille. Il a déjà plongé dans un
+volcan et en est ressorti sans une égratignure. Le personnage parle plus de 20 langues différentes
+mais préfère s’exprimer en tantalogue, la langue de sa planète d’origine.
+https://www.boiteapyjama.com/monde-aime-stitch/
+
+
+</body>
+</html>
