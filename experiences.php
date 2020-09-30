921df78abf0e283b4d53be02477f104981dc1375
Sandra-Louchart <sandra.louchart@gmail.com>
2020-09-30 14:10:17 +0200

    first experience

---
+++ experiences.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<section class="experiences">
+    <h2>Mes expériences:</h2>
+    <div class="experience">
+        <div class="years">
+            <p></p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+
