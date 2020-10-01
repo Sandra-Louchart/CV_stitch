c61254f054d622d7efe1bf10ecb9bee48d92cea8
AmelieRoussel <amelie.roussel41@gmail.com>
2020-10-01 22:36:30 +0200

    compilation des fichiers

--- index.php
+++ index.php
@@ -5,9 +5,50 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style_navbarre.css">
+    <link rel="stylesheet" href="style_footer.css">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
+<header>
+    <nav>
+        <a href="#home" class="home"><img src="img/profil.png" alt="Home"></a>
+        <div class = "nav-nom">
+            <h2>Expérience 626</h2>
+        </div>
+
+        <div class = "menu-items">
+            <div class = "items" id = "menu">
+                <a href="#home" class = "home-burger"><img src="img/profil.png" alt="Home"></a>
+                <a href="#experience">Expériences</a>
+                <a href="#skill">Compétences</a>
+                <a href="#education">Formation</a>
+                <a href="#recommendation">Recommendation</a>
+                <a href="#contact">Contact</a>
+                <a href="#" class="close">X</a>
+            </div>
+            <a class = "burger" href = "#menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </a>
+        </div>
+    </nav>
+</header>
 <body>
 
+
+<footer>
+    <div class = quote>
+        <p>"Ohana" signifie "Famille". </p>
+        <p> Famille signifie que personne ne doit être abandonné ni oublié.</p>
+    </div>
+    <div class="logo">
+        <li><a class="fab fa-facebook-f fa-2x"></a></li>
+        <li><a class="fab fa-twitter fa-2x"></a></li>
+        <li><a class="fab fa-linkedin-in fa-2x"></a></li>
+    </div>
+</footer>
 </body>
+
 </html>
