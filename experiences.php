8b359dc04c11ca6e8f68676597d3b30ca0ed3090
Sandra-Louchart <sandra.louchart@gmail.com>
2020-10-01 16:43:36 +0200

    first test

--- experiences.php
+++ experiences.php
@@ -5,16 +5,57 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-<section class="experiences">
+<section class="experien">
     <h2>Mes expériences:</h2>
     <div class="experience">
-        <div class="years">
-            <p></p>
+        <div class="year">
+            <p>2000-2005</p>
+        </div>
+        <div class ="experiences">
+            <div class="job">
+                <p>Destructeur de la terre</p>
+            </div>
+            <div class="company">
+                <p>kagouroo land</p>
+            </div>
+            <div class="description">
+                <p>.Ultimum ad bella ita ignorantes saevissima vilitate puta abunde quos sed a nec nec eluxisse puta Romana non adsimulata quae.</p>
+            </div>
         </div>
     </div>
+  <!--  <div class="experience">
+        <div class="year">
+            <p>2005-2018</p>
+        </div>
+        <div class="job">
+            <p>chasseur de prime</p>
+        </div>
+        <div class="company">
+            <p>stickland</p>
+        </div>
+        <div class="description">
+            <p>Ultimum ad bella ita ignorantes saevissima vilitate puta abunde quos sed a nec nec eluxisse puta Romana non adsimulata quae.</p>
+        </div>
+    </div>
+    <div class="experience">
+        <div class="year">
+            <p>2018-2020</p>
+        </div>
+        <div class="job">
+            <p>professeur</p>
+        </div>
+        <div class="company">
+            <p>vosio</p>
+        </div>
+        <div class="description">
+            <p>Ultimum ad bella ita ignorantes saevissima vilitate puta abunde quos sed a nec nec eluxisse puta Romana non adsimulata quae.</p>
+        </div>
+    </div>-->
 </section>
 </body>
 </html>
