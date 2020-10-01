bf31f6fd1785c97c84dbfc6c1bfb03d05a007be8
calouvts <pascalevetois@gmail.com>
2020-10-01 15:33:13 +0200

    update file

--- skill.php
+++ skill.php
@@ -5,80 +5,62 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mes études et compétences</title>
+    <title>mes compétences</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
+          rel="stylesheet">
 
 </head>
 
 
 <body>
 
+<div class="alignh2" >
+<h2 class="titreh2"> Mes Compétences</h2>
+</div>
 
-<h1>Mes Compétences</h1>
+<section class="competences">
+    <div class="alignh3">
+    <h3 class="titreh3 "> Mes compétences professionnelles </h3>
+    </div>
+    <div class="skill">
+
+        <div class="skilldetail1">originaire de tantalogue, maitrise de 20 langue différententes, Stitch est
+            particulièrement qualifié
+            pour enseigner les langues étrangères,
+        </div>
+
+        <div class="skilldetail2">intelligence cybernétique, Stitch a toujours un coup d'avance sur les élèves</div>
+        <div class="skilldetail3">avec une vision nocturne une vision par rayons x et infrarouge, Stitch sait repérer les fridandises</div>
+        <div class="skilldetail4">ignifuge et à l'épreuve des balles Sticth est en mesure de faire face à toutes les difficultés que
+            représente une classe de jeunes élèves</div>
 
 
-<section class="compétences">
-    <h2> Mes compétences professionnelles </h2>
-    <div class="professionalskill">
-        <ul>
-            <li>originaire de tantalogue, maitrise de 20 langue différententes, je suis donc particulièrement qualifié pour enseigner les langues étrangères</li>
-            <li>intelligence cybernétique, toujours un coup d'avance sur les élèves/li>
-            <li></li>
-            <li></li>
 
-        </ul>
+    </div>
+
+    <div class="alignh3">
+    <h3 class="titreh3">Mes compétences personnelles</h3>
+    </div>
+    <div class="skill">
+
+        <div class="skilldetail">particulièrement soigneux, et précautionneux Stitch aura a coeur d'assurer la protection physique des
+                élèves
+                </div>
+        <div class="skilldetail"> mature, réfléchis et disposant d'un calme à toute épreuve Stitch saura apporter avec patience et écoute
+                le soutien aux élèves en
+                difficultés</div>
+        <div class="skilldetail">sa participation à des projets d'envergure sur les aménagements planétaires font de Stitch un enseignant
+                à même de conduire les élèves sur des sujets de fonds
+            </div>
+
+
+
+
     </div>
 
 
-    <h2>Mes compétences personnelles</h2>
-
-    <div class=" personnalskill">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-
-        </ul>
-
-    </div>
-    <h2>xxx</h2>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
 </section>
-/*Le prototype 626, plus connu sous le nom de Stitch, est issu d’une expérience génétique illégale
-conduite par Jumba Jookiba et les protagonistes du film d’animation Disney Lilo & Stitch. Il est conçu
-pour être malicieux, et sa fonction première était de détruire tout ce qu’il touchait. Ses pouvoirs incluent
-une immense intelligence et une force phénoménale. Le personnage est ignifuge et à l’épreuve des balles.
-Il est doté de la vision nocturne, la vision par rayon X, et la vision infrarouge.
-Cet inséparable compagnon de Lilo est très apprécié des amoureux de la série.
-Malgré sa force surhumaine et son haut niveau d’intelligence cybernétique, Stitch est émotionnellement fragile
-et une personnalité enfantine se cache sous son extérieur monstrueux. Lilo croit que c’est l’une des raisons pour
-lesquelles,
-il a d’abord poursuivi son œuvre de destruction. Il a été conçu comme un monstre, mais a reçu peu ou pas de
-compréhension et de sympathie
-de la part des autres.
-
-Le personnage s’assagit après son amélioration, mais est bien souvent incapable de contrôler toutes ses pulsions
-et ses actions antisociales. Heureusement pour lui, Lilo (et plus tard Reuben et Angel) est toujours là pour le
-soutenir.
-Les téléspectateurs croient parfois que Lilo est dépendante de Stitch pour sa protection. Au contraire, c’est ce dernier
-qui est dépendant de Lilo.
-Malgré ses difficultés, Lilo fait tout son possible pour aider Stitch. Et à mesure que l’histoire progresse, celui-ci
-devient plus conscient et est
-capable de prendre les bonnes décisions. À la fin de la série, Stitch a des amis, de la famille, des êtres chers et un
-but dans lequel croire*/
-
-
-Tout en expliquant sa création au Conseil Galactique au début de Lilo & Stitch, le Dr Jumba donne le résumé suivant des
-pouvoirs de Stitch: « Il est à l’épreuve des balles, ignifuge, antichoc, et peut penser plus vite qu’un super
-ordinateur. Il voit dans l’obscurité et peut déplacer des objets faisant 3000 fois sa taille. Il a déjà plongé dans un
-volcan et en est ressorti sans une égratignure. Le personnage parle plus de 20 langues différentes
-mais préfère s’exprimer en tantalogue, la langue de sa planète d’origine.
-https://www.boiteapyjama.com/monde-aime-stitch/
 
 
 </body>
