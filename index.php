6d0f8080af0533f313820b003be18ae34c49aa3a
calouvts <pascalevetois@gmail.com>
2020-10-02 11:10:15 +0200

    fusion contact

--- index.php
+++ index.php
@@ -89,6 +89,52 @@
                 </div>
             </div>
         </section>
+        <div class="alignh2" >
+            <h2 class="h2titleskill"> Compétences</h2>
+        </div>
+
+        <section class="competences">
+            <div class="alignh3">
+                <h3 class="h3titleskill "> Professionnelles </h3>
+            </div>
+
+
+            <div class="skill1">
+                <div class="skilldetail1"> <p class="titreskilldetail"> Maîtrise des langues étrangères</p> <p>Originaire de tantalogue, maitrise de 20 langue différententes, Stitch est particulièrement qualifié
+                        pour enseigner les langues</p></div>
+
+                <div class="skilldetail1"> <p class="titreskilldetail"> Intelligence cybernétique</p> <p> Avec son esprit aiguisé Stitch a toujours un coup d'avance sur les élèves</div>
+            </div>
+
+
+            <div class="skill1">
+                <div class="skilldetail1"> <p class="titreskilldetail"> Des capacités visuelles hors normes </p> <p> Grace à une vision nocturne une vision par rayons x et infrarouge, Stitch sait repérer les fridandises, enfin sait être attentif à ce que les élèves manifestent</div>
+                <div class="skilldetail1"> <p class="titreskilldetail"> Une résistance physique à toute épreuve </p> <p>Ignifuge et à l'épreuve des balles Sticth est en mesure de faire face à toutes les difficultés que représente une classe de jeunes élèves</div>
+
+
+            </div>
+
+            <div class="alignh3">
+                <h3 class="h3titleskill">Personnelles</h3>
+            </div>
+            <div class="skill1">
+
+                <div class="skilldetail1"> <p class="titreskilldetail"> Minutie </p> <p>Particulièrement soigneux et précautionneux Stitch aura a coeur d'assurer la protection physique des
+                        élèves</p>
+                </div>
+                <div class="skilldetail1"> <p class="titreskilldetail"> Prestance </p> <p>Mature, réfléchis et disposant d'un calme à toute épreuve Stitch saura apporter avec patience et écoute
+                        le soutien aux élèves en difficultés</p>
+                </div></div>
+
+            <div class="skill1">
+                <div class="skilldetail1"><p class="titreskilldetail">Sait prendre du recul</p><p> Sa participation à des projets d'envergure sur les aménagements planétaires font de Stitch un enseignant
+                        à même de conduire les élèves sur des sujets de fonds</p>
+                </div>
+                <div class="skilldetail1"><p class="titreskilldetail">Gout pour le travail en équipe</p><p> Stitch apprécie tout particulièrement les collaborations actives , Lilo pourra vous fournir toutes recommandations utiles</p>
+                </div>
+
+            </div>
+        </section>
 
 
 
