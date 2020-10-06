461de170666d924db113f3d913ab91521e38f027
AmelieRoussel <amelie.roussel41@gmail.com>
2020-10-06 21:03:16 +0200

    ajout footer

--- index.php
+++ index.php
@@ -6,365 +6,204 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
     <title>Cv de Stitch</title>
 </head>
-    <header>
-        <nav>
-            <a href="#home" class="home"><img src="img/profil.png" alt="Home"></a>
-            <div class = "nav-nom">
-                <h2>Expérience 626</h2>
-            </div>
-
-            <div class = "menu-items">
-                <div class = "items" id = "menu">
-                    <a href="#home" class = "home-burger"><img src="img/profil.png" alt="Home"></a>
-                    <a href="#experience">Expériences</a>
-                    <a href="#skill">Compétences</a>
-                    <a href="#education">Formation</a>
-                    <a href="#recommendation">Recommendation</a>
-                    <a href="#contact" class="contact">Contact</a>
-                    <a href="#" class="close"><img src="img/close.png" alt="close"></a>
-                </div>
-                <a class = "burger" href = "#menu">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </a>
-            </div>
-        </nav>
-
-        <div class="profilePicture">
-            <img src="https://ekladata.com/IN2A71FWEoNqUVd1vseqvGQlqBQ.png" alt="imgProfile" class="imgProfile">
+<header>
+    <nav>
+        <a href="#home" class="home"><img src="img/profil.png" alt="Home"></a>
+        <div class="nav-nom">
+            <h2>Expérience 626</h2>
         </div>
 
-        <div class="nameOfStitch">
-            <h1>Expérience-626 "Stitch"</h1>
-            <h2>Professeur de Français</h2>
+        <div class="menu-items">
+            <div class="items" id="menu">
+                <a href="#home" class="home-burger"><img src="img/profil.png" alt="Home"></a>
+                <a href="#experience">Expériences</a>
+                <a href="#skill">Compétences</a>
+                <a href="#education">Formation</a>
+                <a href="#recommendation">Recommendation</a>
+                <a href="#contact" class="contact">Contact</a>
+                <a href="#" class="close"><img src="img/close.png" alt="close"></a>
+            </div>
+            <a class="burger" href="#menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </a>
         </div>
-        <div class="borderLine">
+    </nav>
 
+    <div class="profilePicture">
+        <img src="https://ekladata.com/IN2A71FWEoNqUVd1vseqvGQlqBQ.png" alt="imgProfile" class="imgProfile">
+    </div>
+
+    <div class="nameOfStitch">
+        <h1>Expérience-626 "Stitch"</h1>
+        <h2>Professeur de Français</h2>
+    </div>
+    <div class="borderLine">
+
+    </div>
+</header>
+<body>
+<section class="experien" id="experience">
+    <h2>Mes expériences:</h2>
+    <div class="experience">
+        <div class="year">
+            <p>2000-2005</p>
         </div>
-    </header>
-    <body>
-        <section class="experien" id="experience">
-            <h2>Mes expériences:</h2>
-            <div class="experience">
-                <div class="year">
-                    <p>2000-2005</p>
-                </div>
-                <div class ="experiences">
-                    <p class="job">Destructeur de la terre</p>
-                    <p class="company">Jumba entreprise</p>
-                </div>
-                <div class="description">
-                    <p>Je suis venu ici pour détruire le monde. J'ai pu créer de nombreux projet pour arriver à mon objectif final...</p>
-                </div>
-            </div>
-            <div class="experience">
-                <div class="year">
-                    <p>2005-2018</p>
-                </div>
-                <div class="experiences">
-                    <p class="job">Chasseur de prime</p>
-                    <p class="company">Hawaï academy</p>
-                </div>
-                <div class="description">
-                    <p>Ne voulant plus quitter la terre, j'ai mis en place une reconversion professionnelle afin d'aider la terre à pouvoir
-                        vivre avec des expériences extrateresstre</p>
-                </div>
-            </div>
-            <div class="experience">
-                <div class="year">
-                    <p>2018-2020</p>
-                </div>
-                <div class="experiences">
-                    <p class="job">Stagiaire professeur des écoles</p>
-                    <p class="company">Education nationale</p>
-                </div>
-                <div class="description">
-                    <p>Après avoir attraper toutes les expériences et survécue à toutes mes aventures j'ai décidé d'apprendre
-                        au plus jeune </p>
-                </div>
-            </div>
-        </section>
-        <div class="alignh2" >
-            <h2 class="h2titleskill"> Compétences</h2>
+        <div class="experiences">
+            <p class="job">Destructeur de la terre</p>
+            <p class="company">Jumba entreprise</p>
+        </div>
+        <div class="description">
+            <p>Je suis venu ici pour détruire le monde. J'ai pu créer de nombreux projet pour arriver à mon objectif
+                final...</p>
+        </div>
+    </div>
+    <div class="experience">
+        <div class="year">
+            <p>2005-2018</p>
+        </div>
+        <div class="experiences">
+            <p class="job">Chasseur de prime</p>
+            <p class="company">Hawaï academy</p>
+        </div>
+        <div class="description">
+            <p>Ne voulant plus quitter la terre, j'ai mis en place une reconversion professionnelle afin d'aider la
+                terre à pouvoir
+                vivre avec des expériences extrateresstre</p>
+        </div>
+    </div>
+    <div class="experience">
+        <div class="year">
+            <p>2018-2020</p>
+        </div>
+        <div class="experiences">
+            <p class="job">Stagiaire professeur des écoles</p>
+            <p class="company">Education nationale</p>
+        </div>
+        <div class="description">
+            <p>Après avoir attraper toutes les expériences et survécue à toutes mes aventures j'ai décidé d'apprendre
+                au plus jeune </p>
+        </div>
+    </div>
+</section>
+<div class="alignh2">
+    <h2 class="h2titleskill"> Compétences</h2>
+</div>
+
+<section class="competences">
+    <div class="alignh3">
+        <h3 class="h3titleskill "> Professionnelles </h3>
+    </div>
+
+
+    <div class="skill1">
+        <div class="skilldetail1"><p class="titreskilldetail"> Maîtrise des langues étrangères</p>
+            <p>Originaire de tantalogue, maitrise de 20 langue différententes, Stitch est particulièrement qualifié
+                pour enseigner les langues</p></div>
+
+        <div class="skilldetail1"><p class="titreskilldetail"> Intelligence cybernétique</p>
+            <p> Avec son esprit aiguisé Stitch a toujours un coup d'avance sur les élèves</div>
+    </div>
+
+
+    <div class="skill1">
+        <div class="skilldetail1"><p class="titreskilldetail"> Des capacités visuelles hors normes </p>
+            <p> Grace à une vision nocturne une vision par rayons x et infrarouge, Stitch sait repérer les fridandises,
+                enfin sait être attentif à ce que les élèves manifestent</div>
+        <div class="skilldetail1"><p class="titreskilldetail"> Une résistance physique à toute épreuve </p>
+            <p>Ignifuge et à l'épreuve des balles Sticth est en mesure de faire face à toutes les difficultés que
+                représente une classe de jeunes élèves</div>
+
+
+    </div>
+
+    <div class="alignh3">
+        <h3 class="h3titleskill">Personnelles</h3>
+    </div>
+    <div class="skill1">
+
+        <div class="skilldetail1"><p class="titreskilldetail"> Minutie </p>
+            <p>Particulièrement soigneux et précautionneux Stitch aura a coeur d'assurer la protection physique des
+                élèves</p>
+        </div>
+        <div class="skilldetail1"><p class="titreskilldetail"> Prestance </p>
+            <p>Mature, réfléchis et disposant d'un calme à toute épreuve Stitch saura apporter avec patience et écoute
+                le soutien aux élèves en difficultés</p>
+        </div>
+    </div>
+
+    <div class="skill1">
+        <div class="skilldetail1"><p class="titreskilldetail">Sait prendre du recul</p>
+            <p> Sa participation à des projets d'envergure sur les aménagements planétaires font de Stitch un enseignant
+                à même de conduire les élèves sur des sujets de fonds</p>
+        </div>
+        <div class="skilldetail1"><p class="titreskilldetail">Gout pour le travail en équipe</p>
+            <p> Stitch apprécie tout particulièrement les collaborations actives , Lilo pourra vous fournir toutes
+                recommandations utiles</p>
         </div>
 
-        <section class="competences">
-            <div class="alignh3">
-                <h3 class="h3titleskill "> Professionnelles </h3>
-            </div>
-
-
-            <div class="skill1">
-                <div class="skilldetail1"> <p class="titreskilldetail"> Maîtrise des langues étrangères</p> <p>Originaire de tantalogue, maitrise de 20 langue différententes, Stitch est particulièrement qualifié
-                        pour enseigner les langues</p></div>
-
-                <div class="skilldetail1"> <p class="titreskilldetail"> Intelligence cybernétique</p> <p> Avec son esprit aiguisé Stitch a toujours un coup d'avance sur les élèves</div>
-            </div>
-
-
-            <div class="skill1">
-                <div class="skilldetail1"> <p class="titreskilldetail"> Des capacités visuelles hors normes </p> <p> Grace à une vision nocturne une vision par rayons x et infrarouge, Stitch sait repérer les fridandises, enfin sait être attentif à ce que les élèves manifestent</div>
-                <div class="skilldetail1"> <p class="titreskilldetail"> Une résistance physique à toute épreuve </p> <p>Ignifuge et à l'épreuve des balles Sticth est en mesure de faire face à toutes les difficultés que représente une classe de jeunes élèves</div>
-
-
-            </div>
-
-            <div class="alignh3">
-                <h3 class="h3titleskill">Personnelles</h3>
-            </div>
-            <div class="skill1">
-
-                <div class="skilldetail1"> <p class="titreskilldetail"> Minutie </p> <p>Particulièrement soigneux et précautionneux Stitch aura a coeur d'assurer la protection physique des
-                        élèves</p>
-                </div>
-                <div class="skilldetail1"> <p class="titreskilldetail"> Prestance </p> <p>Mature, réfléchis et disposant d'un calme à toute épreuve Stitch saura apporter avec patience et écoute
-                        le soutien aux élèves en difficultés</p>
-                </div></div>
-
-            <div class="skill1">
-                <div class="skilldetail1"><p class="titreskilldetail">Sait prendre du recul</p><p> Sa participation à des projets d'envergure sur les aménagements planétaires font de Stitch un enseignant
-                        à même de conduire les élèves sur des sujets de fonds</p>
-                </div>
-                <div class="skilldetail1"><p class="titreskilldetail">Gout pour le travail en équipe</p><p> Stitch apprécie tout particulièrement les collaborations actives , Lilo pourra vous fournir toutes recommandations utiles</p>
-                </div>
-
-            </div>
-        </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <section >
-            <h2>Mes recommendations : </h2>
-            <figure class="reference">
-                <img src="https://vignette.wikia.nocookie.net/disney/images/3/3d/Profile_-_Jumba.jpg/revision/latest/scale-to-width-down/515?cb=20190312074022" alt="Jumba Jookiba">
-                <figcaption>Ma plus belle création, je lui fait confiance</figcaption>
-            </figure>
-            <figure class="reference">
-                <img src="https://vignette.wikia.nocookie.net/disney/images/1/1f/Profile_-_Lilo.png/revision/latest?cb=20190312072611" alt="Lilo Pelekai">
-                <figcaption>J'ai vécue de belles expériences avec lui </figcaption>
-            </figure>
-            <figure class="reference">
-                <img src="https://vignette.wikia.nocookie.net/disney/images/9/9f/Profile_-_Grand_Councilwoman.jpeg/revision/latest/scale-to-width-down/516?cb=20190321135914" alt="Grande conseillère">
-                <figcaption>Je n'aime pas crimnel... Stitch a changé</figcaption>
-            </figure>
-        </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <section class="contact">
-            <h2 id="contact">Ecrivez moi </h2>
-            <form action="mailto:stitch@example.com" method="post">
-                <div>
-                    <label for="name">Nom :</label>
-                    <input type="text" id="name" name="user_name" placeholder="Moi m'appelle stitch">
-                </div>
-                <div>
-                    <label for="email">E-mail :</label>
-                    <input type="email" id="email" name="user_email" placeholder="lilo@example.com">
-                </div>
-                <div>
-                    <label for="msg">Message :</label>
-                    <textarea id="msg" name="user_message" rows="4" cols="50" placeholder="Je m'appelle Stitch...."></textarea>
-                </div>
-                <div class="button">
-                    <button type="submit" id="btn">Envoyez</button>
-                </div>
-            </form>
-        </section>
-
+    </div>
+</section>
+
+
+<section>
+    <h2>Mes recommendations : </h2>
+    <figure class="reference">
+        <img src="https://vignette.wikia.nocookie.net/disney/images/3/3d/Profile_-_Jumba.jpg/revision/latest/scale-to-width-down/515?cb=20190312074022"
+             alt="Jumba Jookiba">
+        <figcaption>Ma plus belle création, je lui fait confiance</figcaption>
+    </figure>
+    <figure class="reference">
+        <img src="https://vignette.wikia.nocookie.net/disney/images/1/1f/Profile_-_Lilo.png/revision/latest?cb=20190312072611"
+             alt="Lilo Pelekai">
+        <figcaption>J'ai vécue de belles expériences avec lui</figcaption>
+    </figure>
+    <figure class="reference">
+        <img src="https://vignette.wikia.nocookie.net/disney/images/9/9f/Profile_-_Grand_Councilwoman.jpeg/revision/latest/scale-to-width-down/516?cb=20190321135914"
+             alt="Grande conseillère">
+        <figcaption>Je n'aime pas crimnel... Stitch a changé</figcaption>
+    </figure>
+</section>
+
+
+<section class="contact">
+    <h2 id="contact">Ecrivez moi </h2>
+    <form action="mailto:stitch@example.com" method="post">
+        <div>
+            <label for="name">Nom :</label>
+            <input type="text" id="name" name="user_name" placeholder="Moi m'appelle stitch">
+        </div>
+        <div>
+            <label for="email">E-mail :</label>
+            <input type="email" id="email" name="user_email" placeholder="lilo@example.com">
+        </div>
+        <div>
+            <label for="msg">Message :</label>
+            <textarea id="msg" name="user_message" rows="4" cols="50" placeholder="Je m'appelle Stitch...."></textarea>
+        </div>
+        <div class="button">
+            <button type="submit" id="btn">Envoyez</button>
+        </div>
+    </form>
+</section>
 
 
 <footer>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <div class=quote>
+        <p class="Ohana">"Ohana" signifie "Famille". </p>
+        <p class="family">Famille signifie que personne ne doit être abandonné, ni oublié.</p>
+    </div>
+    <div class="logo">
+        <li><a class="fab fa-facebook-f fa-2x"></a></li>
+        <li><a class="fab fa-twitter fa-2x"></a></li>
+        <li><a class="fab fa-linkedin-in fa-2x"></a></li>
+    </div>
 </footer>
 </body>
 </html>
