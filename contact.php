e609a55b80a59df0816fe5993b356db9820a0867
AmelieRoussel <amelie.roussel41@gmail.com>
2020-10-07 12:18:02 +0200

    sécurisation formulaire

--- contact.php
+++ contact.php
@@ -1,33 +1,89 @@
-<!DOCTYPE html>
-<html lang="FR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
 <section class="contact">
     <h2 id="contact">Ecrivez moi </h2>
-    <form action="mailto:stitch@example.com" method="post">
+
+    <?php
+    $subjects = [
+        'sujet 1' => 'Demande de renseignements complémentaires',
+        'sujet 2' => 'Demande d\'entretien',
+    ];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+        foreach ($_POST as $name => $input) {
+            $data[$name] = trim($input);
+        }
+
+        if (empty($data['user_name'])) {
+            $errors[] = 'Veuillez remplir votre nom';
+        }
+
+        if (!key_exists($data['subject'], $subjects)) {
+            $errors[] = 'Valeur incorect du sujet';
+        }
+
+        if (empty($data['user_email'])) {
+            $errors[] = 'Veuillez renseigner un email';
+        } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'format de l\'email incorrect';
+        }
+
+        if (!empty($errors)) : ?>
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else :
+            echo 'Merci !'; ?> <br> <?= 'Stitch content';
+        endif;
+    endif;
+    ?>
+
+
+    <form action="" method="post">
         <div>
             <label for="name">Nom :</label>
-            <input type="text" id="name" name="user_name" placeholder="Moi m'appelle stitch">
+            <input type="text"
+                   id="name"
+                   name="user_name"
+                   placeholder="Moi m'appelle stitch"
+                   value=""<?= $data['user_name'] ?? '' ?>
+            >
         </div>
         <div>
             <label for="email">E-mail :</label>
-            <input type="email" id="email" name="user_email" placeholder="lilo@example.com">
+            <input type="email"
+                   id="email"
+                   name="user_email"
+                   placeholder="lilo@example.com"
+                   value=""<?= $data['user_email'] ?? '' ?>
+            >
+        </div>
+        <div>
+            <label for="subject">Sujet</label>
+            <select name="subject" id="subject">
+                <?php foreach ($subjects as $optionValue => $subject) : ?>
+                    <option
+                        <?php if (!empty($data['subject']) && $optionValue === $data['subject']) : ?>
+                            selected
+                        <?php endif; ?>
+                            value="<?= $optionValue ?>">
+                        <?= $subject ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
         </div>
         <div>
             <label for="msg">Message :</label>
-            <textarea id="msg" name="user_message" rows="4" cols="50" placeholder="Je m'appelle Stitch...."></textarea>
+            <textarea id="msg"
+                      name="user_message"
+                      rows="4"
+                      cols="50"
+                      placeholder="Je m'appelle Stitch...."
+                      value=""<?= $data['user_name'] ?? '' ?>
+            ></textarea>
         </div>
         <div class="button">
             <button type="submit" id="btn">Envoyez</button>
         </div>
     </form>
 </section>
-</body>
-</html>
